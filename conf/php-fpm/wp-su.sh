6a8064a74250132b54a658926805a7e59c47044d
Francesco <zerbinati.francesco@gmail.com>
2018-01-20 12:46:49 +0100

    wp-cli fix

--- conf/php-fpm/wp-su.sh
+++ conf/php-fpm/wp-su.sh
@@ -3,4 +3,4 @@
 # Execute wp-cli as the current directory's owner.
 
 THIS_OWNER="$(stat -c %U .)"
-su -m "$THIS_OWNER" -c  "/bin/wp-cli.phar --path='$(pwd)' '$@'"
+su -s "/bin/bash" "$THIS_OWNER" -c "/bin/wp-cli.phar $*"
